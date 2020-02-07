329094763176d9a4e95e8095af5ac915d694710d
Peter Lee <pil206@nyu.edu>
2020-02-06 21:01:12 -0500

    merge-intervals

--- leetcode/merge-intervals.js
+++ leetcode/merge-intervals.js
@@ -1,8 +1,36 @@
 //Given a collection of intervals, merge all overlapping intervals.
 
-//Input: [[1,3],[2,6],[8,10],[15,18]]
+let test = [[1, 3], [2, 6], [8, 10], [15, 18]]
 // Output: [[1,6],[8,10],[15,18]]
 
-// Input: [[1,4],[4,5]]
+let test2 = [[1, 4], [4, 5]]
 // Output: [[1,5]]
 
+function mergeIntervals(intervals) {
+  let results = []
+  let sortedArr = intervals.sort((a, b) => a[0] - b[0])
+  let holder = intervals[0]
+
+  let idx = 1
+
+  if (intervals.length <= 1) {
+    return intervals
+  }
+
+  while (idx !== intervals.length) {
+    let currInt = intervals[idx]
+    if (currInt[0] <= holder[1] && currInt[1] > holder[1]) {
+      holder[1] = currInt[1]
+    }
+    if (currInt[0] > holder[1]) {
+      results.push(holder)
+      holder = currInt
+    }
+    idx++
+  }
+  results.push(holder)
+
+  return results
+}
+
+console.log(mergeIntervals(test2))
