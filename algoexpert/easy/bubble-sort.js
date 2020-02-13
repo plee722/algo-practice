e3ec4abec8f1bceb7f20fed1d72bb43669778ddc
Peter Lee <pil206@nyu.edu>
2020-02-12 20:34:23 -0500

    bubble-sort completed

---
+++ algoexpert/easy/bubble-sort.js
@@ -0,0 +1,14 @@
+// Write a function that takes in an array of integers and returns a sorted version of that array. Use the Bubble Sort algorithm to sort the array.
+
+function bubbleSort(array) {
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      }
+    }
+  }
+  return array
+}
