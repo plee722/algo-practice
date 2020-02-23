6f1e938a770fd16ba8377bbd625f7cabfb5298c1
Peter Lee <pil206@nyu.edu>
2020-02-23 10:54:57 -0500

    symmetric-tree solved

--- leetcode/symmetric-tree.js
+++ leetcode/symmetric-tree.js
@@ -2,42 +2,65 @@
 
 // Passes 194/195 test cases on leetcode.
 
-function isSymmetric(root) {
-  let inOrderDFS = []
+// function isSymmetric(root) {
+//   let inOrderDFS = []
 
-  if (!root) {
+//   if (!root) {
+//     return true
+//   }
+
+//   helper(inOrderDFS, root)
+//   console.log(inOrderDFS)
+
+
+//   let left = 0
+//   let right = inOrderDFS.length - 1
+//   while (left !== right) {
+//     if (inOrderDFS[left] !== inOrderDFS[right]) {
+//       return false
+//     } else {
+//       left++
+//       right--
+//     }
+//   }
+//   return true
+// }
+
+// function helper(arr, tree) {
+//   if (tree.left) {
+//     helper(arr, tree.left)
+//   }
+//   if (!tree.left && tree.right) {
+//     arr.push(null)
+//   }
+//   arr.push(tree.val)
+//   if (tree.right) {
+//     helper(arr, tree.right)
+//   }
+//   if (tree.left && !tree.right) {
+//     arr.push(null)
+//   }
+// }
+
+// Recursive solution that passes all test cases
+function isSymmetric(root) {
+  if (root === null) {
     return true
   }
-
-  helper(inOrderDFS, root)
-  console.log(inOrderDFS)
-
-
-  let left = 0
-  let right = inOrderDFS.length - 1
-  while (left !== right) {
-    if (inOrderDFS[left] !== inOrderDFS[right]) {
-      return false
-    } else {
-      left++
-      right--
-    }
-  }
-  return true
+  return helper(root.left, root.right)
 }
 
-function helper(arr, tree) {
-  if (tree.left) {
-    helper(arr, tree.left)
+function helper(root1, root2) {
+  if (root1 === null && root2 === null) {
+    return true
   }
-  if (!tree.left && tree.right) {
-    arr.push(null)
+  if (root1 === null || root2 === null) {
+    return false
   }
-  arr.push(tree.val)
-  if (tree.right) {
-    helper(arr, tree.right)
+  if (root1.val === root2.val) {
+    if (helper(root1.right, root2.left) && helper(root1.left, root2.right)) {
+      return true
+    }
   }
-  if (tree.left && !tree.right) {
-    arr.push(null)
-  }
+  return false
 }
